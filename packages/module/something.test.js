e670b6793c784000209d5b6053e98ffda9ac65cb
mavu <sickmav@gmail.com>
2020-05-19 15:58:22 +0300

    feat: mizerii

--- packages/module/something.test.js
+++
@@ -1,9 +0,0 @@
-const { log, log2 } = require('./index');
-
-test('log test', () => {
-  expect(log()).toBe('some meaningful message!');
-});
-
-test('log2 test', () => {
-  expect(log2()).toBe('some meani;wlq;lwqpqpwngful message? 21.');
-});
